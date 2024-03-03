7578c6641e32c5b4a0d9075548accb17397492b3
NoamHasson <noam.hasson01@post.runi.ac.il>
2024-03-03 13:43:11 +0200

    Second

--- SpellChecker.java
+++ SpellChecker.java
@@ -20,9 +20,9 @@ public class SpellChecker {
 	} 
 }
 
-	public static int levenshtein(String word1, String word2) { // hello(5) hpll(4)
-		word1.toLowerCase(); 
-		word2.toLowerCase();
+	public static int levenshtein(String word1, String word2) { 
+		word1 = word1.toLowerCase(); 
+		word2 = word2.toLowerCase();
 
 		if ( word1.length() == 0 ) {
 			return word2.length();
